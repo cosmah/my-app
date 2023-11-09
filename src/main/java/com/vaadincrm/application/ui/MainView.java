d702856ff8029cfee347c340be99d014c9adc9c7
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-11-09 14:25:26 +0300

    ADDED TABLE

--- src/main/java/com/vaadincrm/application/ui/MainView.java
+++ src/main/java/com/vaadincrm/application/ui/MainView.java
@@ -2,18 +2,32 @@ package com.vaadincrm.application.ui;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadincrm.application.backend.entity.Contact;
 
 
 //1. FIRST DEFINE ENTRY ROUTE
 @Route("")
 public class MainView extends VerticalLayout {
 
-    //2. Set up component
-    public MainView(){
+    //define UI
+  private Grid<Contact> grid = new Grid<>(Contact.class); 
+  public MainView() {
+  addClassName("list-view"); 
+  setSizeFull(); 
+  configureGrid(); 
+  add(grid); 
+  }
+  private void configureGrid() {
+  grid.addClassName("contact-grid");
+  grid.setSizeFull();
+  grid.setColumns("firstName", "lastName", "email", "status"); 
+  }
 
-    }
+  //------TABLE-----
+
 }
